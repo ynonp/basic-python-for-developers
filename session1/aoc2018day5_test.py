37473a0f9317b5cf4c090b94deaabfd8c3a965bb
ynonp <ynonperek@gmail.com>
2020-11-18 13:16:36 +0200

    add pytest and polymer example

---
+++ session1/aoc2018day5_test.py
@@ -0,0 +1,21 @@
+from aoc2018day5 import reduce_polymer
+
+
+def test_aA():
+    assert reduce_polymer("aA") == ""
+
+
+def test_abBA():
+    assert reduce_polymer("abBA") == ""
+
+
+def test_abAB():
+    assert reduce_polymer("abAB") == "abAB"
+
+
+def test_dabAcCaCBAcCcaDA():
+    assert reduce_polymer("dabAcCaCBAcCcaDA") == "dabCBAcaDA"
+
+
+
+
