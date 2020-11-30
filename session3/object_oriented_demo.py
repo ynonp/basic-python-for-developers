f9ee4e70915759030a842a7133d7551e02e41069
ynonp <ynonperek@gmail.com>
2020-11-30 11:28:50 +0200

    add object oriented exercises

--- session3/object_oriented_demo.py
+++ session3/object_oriented_demo.py
@@ -20,6 +20,52 @@ print(x)
 ############ Object Oriented Syntax
 
 class Critter:
+    def __init__(self):
+        self.size = 0
+
+    def eat(self):
+        self.size += 1
+        print(f"Yummy I already ate {self.size} portions")
+
+    def die(self):
+        print("XXX")
+
+
+c = Critter()
+d = Critter()
+
+c.eat()
+c.eat()
+c.eat()
+
+
+Critter.eat(d)
+d.eat()
+
+print(c.size)
+
+
+###############################
+
+
+class Players:
+    names = ['bill', 'cliff']
+
+    def add_player(self, name):
+        self.names.append(name)
+
+
+p = Players()
+r = Players()
+
+p.add_player('ynon')
+print(r.names)
+
+
+
+
+
+
 
 
 
