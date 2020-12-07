7f4085779ce3e79006a1f51fbe0587c231ff9f8f
ynonp <ynonperek@gmail.com>
2020-12-07 11:44:04 +0200

    solved regexp lab

--- session4/lab1.py
+++ session4/lab1.py
@@ -1,3 +1,16 @@
+# Lab:
+
+# Get the program below to print only the
+# NAME and IP ADDRESS of each network interface
+
+# Regexp for ANYTHING (including newlines)
+# "(\n|.)*"
+# "[\n\w\W]*"
+# \w - [a-zA-Z0-9_]
+
+import re
+pattern = "(?P<name>[-\w]+):[\n\w\W]*inet (?P<ip>[\d.]+)[\n\w\W]*"
+
 interfaces = []
 with open('regexp_lab.txt', 'r') as f:
     data = f.read()
@@ -7,4 +20,6 @@ with open('regexp_lab.txt', 'r') as f:
 print(f"There are {len(interfaces)} network interfaces")
 
 for ith in interfaces:
-    print(ith)
+    match = re.fullmatch(pattern, ith)
+    if match:
+        print(match.groupdict())
